ac65d8707d27c4e0600869a1e445aaf38ee4aa01
Kuba <58139870+jakubjereczek@users.noreply.github.com>
2024-02-22 17:28:16 +0100

    build: set up typescript types declaration

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,10 @@
 module.exports = {
-  "trailingComma": "all",
-  "tabWidth": 2,
-  "semi": true,
-  "singleQuote": true,
-  "printWidth": 80,
-  "bracketSpacing": true
-}
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  bracketSpacing: true,
+};
+
+// logs capture
